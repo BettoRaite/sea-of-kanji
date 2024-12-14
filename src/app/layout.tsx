724b018cc6d9b30bc21b56e3a8d1436c29132ffe
bettoraite <bettoraite@gmail.com>
2024-12-14 20:19:15 +0500

    refactor: removed unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import { BottomMenu } from "@/components/BottomMenu/BottomMenu";
-import { Provider } from "react-redux";
-import { store } from "@/redux/store";
 
 export const metadata: Metadata = {
   title: "My App",
